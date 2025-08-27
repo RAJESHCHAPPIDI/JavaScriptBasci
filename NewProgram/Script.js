388ff074411d7e6ffcd5e072bb143e0aeca5bdf6
RAJESHCHAPPIDI <RAJESH.CHAPPIDI086@GMAIL.COM>
2025-08-27 06:00:52 +0530

    save

---
+++ NewProgram/Script.js
@@ -0,0 +1,15 @@
+let name="Rajesh"
+let age=39
+
+console.log(`${name}'s age is ${age}`)
+console.log(age+1);
+console.log(`${name}'s age is ${age}`);
+age=age+1//Binary operator
+console.log(`${name}'s age is ${age}`);
+++age//Unary operator
+console.log(`${name}'s age is ${age}`);
+//type of
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof typeof age)
+
